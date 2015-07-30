5e492f8babe31e18e59f4a6b0bd1b3da63c1f024
scensorECHO <xxartofwarxx@gmail.com>
2015-07-30 11:54:00 -0400

    Scaled to terminal size

--- pcmr.py
+++ pcmr.py
@@ -1,12 +1,27 @@
-print("\n\n\n\n\n\n\n\n\n")
-print("                     ██▓███   ▄████▄   ███▄ ▄███▓ ██▀███  ")
-print("                    ▓██░  ██▒▒██▀ ▀█  ▓██▒▀█▀ ██▒▓██ ▒ ██▒")
-print("                    ▓██░ ██▓▒▒▓█    ▄ ▓██    ▓██░▓██ ░▄█ ▒")
-print("                    ▒██▄█▓▒ ▒▒▓▓▄ ▄██▒▒██    ▒██ ▒██▀▀█▄  ")
-print("                    ▒██▒ ░  ░▒ ▓███▀ ░▒██▒   ░██▒░██▓ ▒██▒")
-print("                    ▒▓▒░ ░  ░░ ░▒ ▒  ░░ ▒░   ░  ░░ ▒▓ ░▒▓░")
-print("                    ░▒ ░       ░  ▒   ░  ░      ░  ░▒ ░ ▒░")
-print("                    ░░       ░        ░      ░     ░░   ░ ")
-print("                             ░ ░             ░      ░     ")
-print("                             ░                            ")
-print("\n\n\n")
+import os
+rowS, columnS = os.popen('stty size', 'r').read().split()
+row = int(rowS)
+col = int(columnS)
+
+pcmr = []
+pcmr.append(' ██▓███   ▄████▄   ███▄ ▄███▓ ██▀███  ')
+pcmr.append('▓██░  ██▒▒██▀ ▀█  ▓██▒▀█▀ ██▒▓██ ▒ ██▒')
+pcmr.append('▓██░ ██▓▒▒▓█    ▄ ▓██    ▓██░▓██ ░▄█ ▒')
+pcmr.append('▒██▄█▓▒ ▒▒▓▓▄ ▄██▒▒██    ▒██ ▒██▀▀█▄  ')
+pcmr.append('▒██▒ ░  ░▒ ▓███▀ ░▒██▒   ░██▒░██▓ ▒██▒')
+pcmr.append('▒▓▒░ ░  ░░ ░▒ ▒  ░░ ▒░   ░  ░░ ▒▓ ░▒▓░')
+pcmr.append('░▒ ░       ░  ▒   ░  ░      ░  ░▒ ░ ▒░')
+pcmr.append('░░       ░        ░      ░     ░░   ░ ')
+pcmr.append('         ░ ░             ░      ░     ')
+pcmr.append('         ░                            ')
+
+pcmrW = len(pcmr[0])
+pcmrH = len(pcmr)
+
+fillerW = int((col - pcmrW)/2)
+fillerH = int((row - pcmrH)/2)
+
+print('\n'*fillerH)
+for line in pcmr:
+	print(' '*fillerW + line)
+print('\n'*fillerH)
