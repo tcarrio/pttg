d5f73a1b239353303e0d11e2ab3697d8541fea7c
Tom Carrio <xxartofwarxx@gmail.com>
2015-07-30 13:25:32 -0400

    Added Feral ASCII
    Ode to Feral, Screenshot to go up tonight with Shadow of Mordor gameplay

---
+++ feral.py
@@ -0,0 +1,36 @@
+import os
+rowS, columnS = os.popen('stty size', 'r').read().split()
+row = int(rowS)
+col = int(columnS)
+
+feral = []
+feral.add("   █████▒▓█████  ██▀███   ▄▄▄       ██▓      ")
+feral.add(" ▓██   ▒ ▓█   ▀ ▓██ ▒ ██▒▒████▄    ▓██▒      ")
+feral.add(" ▒████ ░ ▒███   ▓██ ░▄█ ▒▒██  ▀█▄  ▒██░      ")
+feral.add(" ░▓█▒  ░ ▒▓█  ▄ ▒██▀▀█▄  ░██▄▄▄▄██ ▒██░      ")
+feral.add(" ░▒█░    ░▒████▒░██▓ ▒██▒ ▓█   ▓██▒░██████▒  ")
+feral.add("  ▒ ░    ░░ ▒░ ░░ ▒▓ ░▒▓░ ▒▒   ▓▒█░░ ▒░▓  ░  ")
+feral.add("  ░       ░ ░  ░  ░▒ ░ ▒░  ▒   ▒▒ ░░ ░ ▒  ░  ")
+feral.add("  ░ ░       ░     ░░   ░   ░   ▒     ░ ░     ")
+feral.add("            ░  ░   ░           ░  ░    ░  ░  ")
+feral.add("                                             ")
+feral.add("  ▄████  ▄▄▄       ███▄ ▄███▓▓█████   ██████ ")
+feral.add(" ██▒ ▀█▒▒████▄    ▓██▒▀█▀ ██▒▓█   ▀ ▒██    ▒ ")
+feral.add("▒██░▄▄▄░▒██  ▀█▄  ▓██    ▓██░▒███   ░ ▓██▄   ")
+feral.add("░▓█  ██▓░██▄▄▄▄██ ▒██    ▒██ ▒▓█  ▄   ▒   ██▒")
+feral.add("░▒▓███▀▒ ▓█   ▓██▒▒██▒   ░██▒░▒████▒▒██████▒▒")
+feral.add(" ░▒   ▒  ▒▒   ▓▒█░░ ▒░   ░  ░░░ ▒░ ░▒ ▒▓▒ ▒ ░")
+feral.add("  ░   ░   ▒   ▒▒ ░░  ░      ░ ░ ░  ░░ ░▒  ░ ░")
+feral.add("░ ░   ░   ░   ▒   ░      ░      ░   ░  ░  ░  ")
+feral.add("      ░       ░  ░       ░      ░  ░      ░  ")
+
+feralW = len(feral[0])
+feralH = len(feral)
+
+fillerW = int((col - feralW)/2)
+fillerH = int((row - feralH)/2)
+
+print('\n'*fillerH)
+for line in feral:
+	print(' '*fillerW + line)
+print('\n'*fillerH)
